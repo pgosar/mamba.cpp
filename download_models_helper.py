53d8216cd7372b9cc1587704ff0b320e374315e3
pgosar <pgosarsteam@gmail.com>
2024-03-10 10:30:03 -0500

    download all 3 models

--- download_models_helper.py
+++ download_models_helper.py
@@ -3,6 +3,13 @@ import torch
 
 tokenizer = AutoTokenizer.from_pretrained("state-spaces/mamba-2.8b-hf")
 model = MambaForCausalLM.from_pretrained("state-spaces/mamba-2.8b-hf")
+
+tokenizer = AutoTokenizer.from_pretrained("state-spaces/mamba-130m-hf")
+model = MambaForCausalLM.from_pretrained("state-spaces/mamba-130m-hf")
+
+tokenizer = AutoTokenizer.from_pretrained("state-spaces/mamba-1.4b-hf")
+model = MambaForCausalLM.from_pretrained("state-spaces/mamba-1.4b-hf")
+
 input_ids = tokenizer("Hey how are you doing?", return_tensors="pt")["input_ids"]
 out = model.generate(input_ids, max_new_tokens=10)
 print(tokenizer.batch_decode(out))
